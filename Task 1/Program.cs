4c8dc067983bd7bcee8f9d0c74a650335c211c84
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-02 08:47:39 +0545

    added task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Task_1;
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Rectangle rectangle = new Rectangle();
+        rectangle.Length = 5;
+        rectangle.Breadth = 3;
+        Console.WriteLine(rectangle.ShowDetails());
+        Console.WriteLine(rectangle.GetArea());
+        Console.WriteLine(rectangle.GetPerimeter());
+    }
+}
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,25 @@
+namespace Task_1;
+
+public class Rectangle
+{
+    private double length;
+    private double breadth;
+
+    public double Length
+    {
+        get => length; 
+        set => length = value; 
+    }
+    
+    public double Breadth
+    {
+        get => breadth;
+        set => breadth = value;
+    }
+
+    public double GetArea() =>  length * breadth;
+
+    public double GetPerimeter() => length * 2 + breadth * 2;
+
+    public string ShowDetails() => $"Length: {length} breadth: {breadth}";
+}
