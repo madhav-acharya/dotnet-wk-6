e47fa5bbb06b5e9e104c343066c2c26a77d64047
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-03 10:50:52 +0545

    completed task 3

--- Task 1/obj/Debug/net9.0/Task 1.AssemblyInfo.cs
+++ Task 1/obj/Debug/net9.0/Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c464ff7b9e3d6c5eed180edbebb3f2c32e5e2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task 3/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Task_3;
+
+public class Program
+{
+    static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+    {
+        foreach (var num in numbers)
+        {
+            if (condition(num))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 4, 7, 12, 15, 8, 22 };
+
+        Console.WriteLine("Even numbers:");
+        // Passing a delegate to check for even numbers
+        ProcessNumbers(numbers, n => n % 2 == 0);
+
+        Console.WriteLine("\nNumbers greater than 10:");
+        // Passing a delegate to check for numbers greater than 10
+        ProcessNumbers(numbers, n => n > 10);
+    }
+}
